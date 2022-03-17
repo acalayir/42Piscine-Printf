a4031f1311286c9c5fdc81d64dc82cf21bc0bd44
acalayir <101639800+acalayir@users.noreply.github.com>
2022-03-17 15:24:09 +0300

    Add files via upload

---
+++ ft_printf/ft_print_hex.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_hex.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acalayir <42istanbul.com.tr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 16:31:55 by acalayir          #+#    #+#             */
+/*   Updated: 2022/01/26 12:13:17 by acalayir         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_hex_len(unsigned	int num)
+{
+	int	len;
+
+	len = 0;
+	while (num != 0)
+	{
+		len++;
+		num = num / 16;
+	}
+	return (len);
+}
+
+void	ft_put_hex(unsigned int num, const char format)
+{
+	if (num >= 16)
+	{
+		ft_put_hex(num / 16, format);
+		ft_put_hex(num % 16, format);
+	}
+	else
+	{
+		if (num <= 9)
+			ft_printchar(((int)num) + '0');
+		else
+		{
+			if (format == 'x')
+				ft_printchar(((int)num) - 10 + 'a');
+			if (format == 'X')
+				ft_printchar(((int)num) - 10 + 'A');
+		}
+	}
+}
+
+int	ft_print_hex(unsigned int num, const char format)
+{
+	if (num == 0)
+		return (write(1, "0", 1));
+	else
+		ft_put_hex(num, format);
+	return (ft_hex_len(num));
+}
---
+++ ft_printf/ft_printf_utils.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acalayir <42istanbul.com.tr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 16:34:00 by acalayir          #+#    #+#             */
+/*   Updated: 2022/01/26 12:38:21 by acalayir         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printchar(int c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_printstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str == NULL)
+	{
+		write(1, "(null)", 6);
+		return (6);
+	}
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	return (i);
+}
+
+int	ft_printpercent(void)
+{
+	write(1, "%", 1);
+	return (1);
+}
